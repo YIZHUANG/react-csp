498a179070dc585cf2d7ef7269003ff5ed8a90da
Viktor Sarström <viktorsarstrom@gmail.com>
2021-08-02 10:57:24 +0200

    Added filename as an argument

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,17 @@
 import { ConfigExtension } from "./types";
 
+
+let filename = "index.html";
+const args = process.argv.slice(2);
+
+if (Array.isArray(args) && args[1] !== undefined) {
+  filename = args[1];
+  console.log(`Using ${args[1]} as the file name`);
+} 
+
 const baseDir = process.cwd();
 const configPath = (extension: ConfigExtension) =>
   process.cwd() + `/csp.${extension}`;
-const htmlPath = process.cwd() + "/public/index.html";
+const htmlPath = process.cwd() + `/public/${filename}`;
 
-export { baseDir, configPath, htmlPath };
+export { baseDir, configPath, htmlPath, filename };
