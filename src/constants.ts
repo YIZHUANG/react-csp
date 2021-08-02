fdf0bb5d3fb01f490448465d0754bbbf329faa91
Viktor Sarström <viktorsarstrom@gmail.com>
2021-08-02 11:18:34 +0200

    Loop through the variables

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,15 @@
 import { ConfigExtension } from "./types";
 
-
 let filename = "index.html";
 const args = process.argv.slice(2);
 
-if (Array.isArray(args) && args[1] !== undefined) {
-  filename = args[1];
-  console.log(`Using ${args[1]} as the file name`);
-} 
+args.forEach(function (value, index) {
+  if(value === '--filename' || value === '--f') {
+    if(args[index+1]) {
+      filename = args[index+1];
+    }
+  }
+});
 
 const baseDir = process.cwd();
 const configPath = (extension: ConfigExtension) =>
